e55a52787ec1bd2461ee0b0ac482c7afaff2ca46
kijima <norimaking777@gmail.com>
2021-04-12 09:15:07 +0900

    Done 5.3

---
+++ CH5/car_replace.rb
@@ -0,0 +1,47 @@
+# replaced by instance variable @total_count
+
+class Car
+  @@makes = []
+  @@cars = {}
+  attr_reader :make
+
+  def self.total_count
+    @total_count ||= 0
+  end
+
+  def self.total_count=(n)
+    @total_count = n
+  end
+
+  def self.add_make(make)
+    unless @@makes.include?(make)
+      @@makes << make
+      @@cars[make] = 0
+    end
+  end
+
+  def initialize(make)
+    if @@makes.include?(make)
+      puts "Creating a new #{make}!"
+      @make = make
+      @@cars[make] += 1
+      self.class.total_count += 1
+    else
+      raise "No such make: #{make}."
+    end
+  end
+
+  def make_mates
+    @@cars[self.make]
+  end
+end
+
+class Hybrid < Car
+end
+
+Car.add_make("Honda")
+Car.add_make("Ford")
+
+h3 = Hybrid.new("Honda")
+f2 = Hybrid.new("Ford")
+puts "There are #{Hybrid.total_count} hybrids on the road!"
