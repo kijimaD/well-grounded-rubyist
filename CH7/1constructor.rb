506ca3ea50bf354617d6fc4078970ae5f2e93ac2
kijima <norimaking777@gmail.com>
2021-04-18 09:52:24 +0900

    Done 7.1

---
+++ CH7/1constructor.rb
@@ -0,0 +1,18 @@
+# Most of class can be instantiated using new.
+str = String.new
+arr = Array.new
+
+# Some can't. you can't create a new instance of the class Integer.
+
+# Literal constructors
+# String -> "new string"
+# Symbol -> :symbol
+# Array -> [1, 2, 3, 4, 5]
+# Hash -> { "New York" => "NY" }
+# Range -> 0..9
+# Regexp -> /([a-z]+)/
+# Proc(lambda) → ->(x, y { x * y }
+
+
+# Literal constructor characters with more than one meaning...
+# ex. curly braces... Codeblock + Hash
